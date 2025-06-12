41d250920b6320965d71890e6f1850680bc00726
alfredopizana <pizana.alfredo@gmail.com>
2025-06-11 22:49:44 -0600

    Changes for friends party (#1)
    Co-authored-by: Sofia Hernandez <sofy.hernandeza@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import CustomButton from './components/CustomButton'
 import ImageButton from './components/ImageButton'
 import liverpoolLogo from './assets/images/liverpool.png'
 import amazonLogo from './assets/images/amazon.png'
-// import footer from './assets/images/footer.png'
 import { Image } from 'react-bootstrap'
 
 import eventDetailImage from './assets/images/event-details-divider.png'
@@ -16,7 +15,7 @@ function App() {
     <>
       <link
         itemProp="thumbnailUrl"
-        href="https://baby-shower-self.vercel.app/cover_image.png"
+        href="https://baby-shower-self.vercel.app/cover_image.jpeg"
       />
       <span
         itemProp="thumbnail"
@@ -25,7 +24,7 @@ function App() {
       >
         <link
           itemProp="url"
-          href="https://baby-shower-self.vercel.app/cover_image.png"
+          href="https://baby-shower-self.vercel.app/cover_image.jpeg"
         />
       </span>
       <HeroBanner />
@@ -37,23 +36,23 @@ function App() {
         <div className="d-flex flex-column flex-md-row text-center event-details">
           <EventDetail
             title="¿Cuándo?"
-            text="Sabado 12 de Julio, 2025<br/>a partir de 2:30pm"
+            text="Sabado 5 de Julio, 2025<br/>a partir de 3:00pm"
             styleName="when"
-            link="https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20250712T203000Z%2F20250713T020000Z&location=https%3A%2F%2Fmaps.app.goo.gl%2Ft63jB4Ae3Xozdua29&text=Baby%20Shower%20Flora"
+            link="https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20250705T210000Z%2F20250706T040000Z&location=https%3A%2F%2Fwww.google.com.mx%2Fmaps%2Fplace%2FJardines%2Bde%2Blos%2BCerezos%2B1840%2C%2BJardines%2Bdel%2BVergel%2C%2B45130%2BZapopan%2C%2BJal.%2F%4020.7530663%2C-103.3729022%2C17z%2Fdata%3D%214m6%213m5%211s0x8428aff7141b05cd%3A0x322d8c6f1030fe5b%218m2%213d20.7531566%214d-103.3720546%2116s%252Fg%252F11swtdnrsw%3Fentry%3Dttu%26g_ep%3DEgoyMDI1MDYxMC4wIKXMDSoASAFQAw%253D%253D&text=Baby%20Shower%20Mar%C3%ADa%20Julieta"
             buttonText="Agregar a mi calendario"
             icon="calendar"
           />
           <EventDetail
             title="¿En dónde?"
-            text="Terraza Berath"
+            text="Jardines de los Cerezos 1840"
             styleName="where"
-            link="https://maps.app.goo.gl/t63jB4Ae3Xozdua29"
-            buttonText="Sta. Fe 553, La Tuzania, Zapopan."
+            link="https://www.google.com.mx/maps/place/Jardines+de+los+Cerezos+1840,+Jardines+del+Vergel,+45130+Zapopan,+Jal./@20.7530663,-103.3729022,17z/data=!4m6!3m5!1s0x8428aff7141b05cd:0x322d8c6f1030fe5b!8m2!3d20.7531566!4d-103.3720546!16s%2Fg%2F11swtdnrsw?entry=ttu&g_ep=EgoyMDI1MDYxMC4wIKXMDSoASAFQAw%3D%3D"
+            buttonText="Jardines de los Cerezos 1840, Jardines del Vergel, Zapopan."
             icon="map"
           />
         </div>
         <CustomButton
-          link="https://forms.gle/7GkCuJEFUfvxXPFn9"
+          link="https://docs.google.com/forms/d/e/1FAIpQLSecxE8krPrgJUkDsi4cOrNR-qaVRUZAXFY2fSg7vC9b3iYbGQ/viewform"
           text="Confirmar asistencia"
           buttonVariant=""
           buttonClass="rsvp btn-primary "
@@ -77,7 +76,7 @@ function App() {
         <div className="d-flex flex-column flex-md-row text-center justify-content-center gap-3">
           <a
             className="baby-registry-card"
-            href="https://mesaderegalos.liverpool.com.mx/milistaderegalos/51654538"
+            href="https://mesaderegalos.liverpool.com.mx/milistaderegalos/51702236"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -99,7 +98,7 @@ function App() {
 
           <a
             className="baby-registry-card"
-            href="https://www.amazon.com.mx/baby-reg/jesusalfredopizaa-espinosa-sofy-hdz-septiembre-2025-zapopan/3OTNG2VC888U9"
+            href="https://www.amazon.com.mx/baby-reg/carlosandres-diaz-agosto-2025-zapopan/1W6T4K8K8C2TH"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -118,17 +117,7 @@ function App() {
               <p className="card-text">Ir a la mesa de Regalos </p>
             </div>
           </a>
-        </div>
-        <p className="mt-3 w-80 text-center">
-          <em>
-            <small>
-              Nota de los papás: Notarás que no hemos incluido pañales
-              desechables en la lista. Esto se debe a que hemos optado por una
-              alternativa más amigable con el medio ambiente. ¡Gracias por tu
-              comprensión y apoyo!
-            </small>
-          </em>
-        </p>
+        </div>        
       </div>
       <hr className="w-75 separator" />
       <section className="container questions">
@@ -143,15 +132,15 @@ function App() {
             </small>
           </em>
           <CustomButton
-            link="https://api.whatsapp.com/send?phone=3316982226"
-            text="33 1698 2226 (Alfredo)"
+            link="https://api.whatsapp.com/send?phone=3334030204"
+            text="33 3403 0204 (Carlos)"
             buttonVariant="outline-dark"
             buttonClass="mt-4"
             icon="whatsapp"
           />
           <CustomButton
-            link="https://api.whatsapp.com/send?phone=3316930366"
-            text="33 1693 0366 (Sofy)"
+            link="https://api.whatsapp.com/send?phone=3315347563"
+            text="33 1534 7563 (Paty)"
             buttonVariant="outline-dark"
             buttonClass="mt-4"
             icon="whatsapp"
@@ -163,7 +152,7 @@ function App() {
       <footer>
         <p>
           ¡Esperamos celebrar contigo! Gracias por ser parte de este día tan
-          especial. | 2025 Alfred & Sofy
+          especial. | 2025 Carlos & Paty
         </p>
       </footer>
     </>
